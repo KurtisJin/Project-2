282bc2f55d441b550179337a52038fbc7cd0f119
Jakers811 <akerstjustin@gmail.com>
2021-05-14 15:20:59 -0500

    Added for loop to remove events w/o desc.

--- Controllers/resultsRoutes.js
+++ Controllers/resultsRoutes.js
@@ -2,21 +2,33 @@ const axios = require('axios');
 const router = require('express').Router();
 
 router.get('/:query', async (req, res) => {
-  const eventData = await axios.get('https://app.ticketmaster.com/discovery/v2/events?apikey=pETvCuGAevOjovqF0cqFbAly9fYBD9vZ&keyword=fire&locale=*');
-  console.log(eventData);
-  let queryResult = eventData.data._embedded.events;
-  let festivals = queryResult.map((event) => {
+  var searchResult;
+  try {
+    searchResult = await axios.get('https://app.ticketmaster.com/discovery/v2/events?apikey=pETvCuGAevOjovqF0cqFbAly9fYBD9vZ&keyword=fire&locale=en-us&page=2');
+  } catch (err) {
+    res.status(400).json(err);
+  }
+
+  searchResult = searchResult.data._embedded.events;
+  let filteredEvents = [];
+
+  searchResult.forEach(events => {
+    if ('description' in events) {
+      filteredEvents.push(events);
+    }
+  });
+
+  let festivals = filteredEvents.map((event) => {
     return {
       name: event.name,
       summary: event.description,
       logo: event.images[0]
     }
   })
+
   res.render("results", {
     festivals
   })
 });
-
-
 
 module.exports = router;
