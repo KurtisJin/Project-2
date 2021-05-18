7d88ceae92cac7376162a47e3a5700225a7f5163
Jakers811 <akerstjustin@gmail.com>
2021-05-17 19:50:52 -0500

    changed little stuff

--- models/Festival.js
+++ models/Festival.js
@@ -7,47 +7,45 @@ const sequelize = require('../config/connection');
 class Festival extends Model {}
 
 
-Festival.init(
-  {
-    id: {
-      type: DataTypes.INTEGER,
-      allowNull: false,
-      primaryKey: true,
-      autoIncrement: true,
-    },
-    name: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
-    description: {
-      type: DataTypes.STRING,
-    },
-    date_created: {
-      type: DataTypes.DATE,
-      allowNull: false,
-      defaultValue: DataTypes.NOW,
-    },
-    needed_funding: {
-      type: DataTypes.FLOAT,
-      allowNull: false,
-    },
-    lineup: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
-  
-    image: {
-      default: ''
-    },  
-    
-    user_id: {
-      type: DataTypes.INTEGER,
-      references: {
-        model: 'user',
-        key: 'id',
-      },
+Festival.init({
+  id: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    primaryKey: true,
+    autoIncrement: true,
+  },
+  name: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  description: {
+    type: DataTypes.STRING,
+  },
+  date_created: {
+    type: DataTypes.DATE,
+    allowNull: false,
+    defaultValue: DataTypes.NOW,
+  },
+  needed_funding: {
+    type: DataTypes.FLOAT,
+    allowNull: false,
+  },
+  lineup: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
 
+  // image: {
+  //   default: ''
+  // },
+
+  user_id: {
+    type: DataTypes.INTEGER,
+    references: {
+      model: 'user',
+      key: 'id',
     },
+
   },
 }, {
   sequelize,
