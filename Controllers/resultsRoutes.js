d0e12fa44ddba37e1ab68f880a8a5706757989bc
Jakers811 <akerstjustin@gmail.com>
2021-05-17 21:36:48 -0500

    changed password, added route stuff blah blah

--- Controllers/resultsRoutes.js
+++ Controllers/resultsRoutes.js
@@ -1,16 +1,14 @@
 const axios = require('axios');
+const { response } = require('express');
 const router = require('express').Router();
 
 router.get('/:query/:page?', async (req, res) => {
   var searchResult;
   const page = req.params.page ? req.params.page : 1;
   try {
-    searchResult = await axios.get('https://app.ticketmaster.com/discovery/v2/events?apikey=pETvCuGAevOjovqF0cqFbAly9fYBD9vZ&keyword=' + req.params.query + '&locale=en-us&segmentName=music&page=' + page);
-  } catch (err) {
-    res.status(400).json(err);
-  }
-
-  searchResult = searchResult.data._embedded.events;
+   const response = await axios.get('https://app.ticketmaster.com/discovery/v2/events?apikey=pETvCuGAevOjovqF0cqFbAly9fYBD9vZ&keyword=' + req.params.query + '&locale=en-us&segmentName=music&page=' + page);
+   console.log(response);
+  searchResult = response.data._embedded.events;
   console.log(searchResult[0].classifications);
   let filteredEvents = searchResult.filter(events => {
     return ('name' in events)
@@ -24,9 +22,27 @@ router.get('/:query/:page?', async (req, res) => {
     }
   });
 
+  // [
+  //  {
+  //    index:1,
+  //    href:"/results/fire/1"
+  //   },
+  //   "/results/fire/2",
+  //   "/results/fire/3",
+  //   "/results/fire/4",
+  // ];
+  let resultsPages = [];
+  for (let index = 1; index <= response.data.page.totalPages; index++) {
+    resultsPages.push({index:index,href:"/results/"+req.params.query+"/"+index});
+  }
+  console.log(resultsPages);
   res.render("results", {
-    festivals
+    festivals,
+    resultsPages
   })
+  } catch (err) {
+    res.status(400).json(err);
+  }
 });
 
 module.exports = router;
