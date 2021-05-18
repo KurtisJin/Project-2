91547f3b43e4c5b6ceea9d1e202367ce2c14d238
kurtisjin <cloudnine21@gmail.com>
2021-05-17 20:34:48 -0500

    fixed homepageroute, tested and fixed festrival route

--- Controllers/homepageRoutes.js
+++ Controllers/homepageRoutes.js
@@ -7,18 +7,18 @@ router.get('/', async (req, res) => {
   try {
     // Get all festivals and JOIN with user data
     const festivalData = await Festival.findAll({
-      include: [User,
-        {
-          model: Festival,
-          attributes:['id', 'name', 'description', 'date_created', 'need_funding', 'lineup'],
-          include: {
-            attributes: ['name'],
-          }
+      // include: [User,
+      //   {
+      //     model: Festival,
+      //     attributes:['id', 'name', 'description', 'date_created', 'need_funding', 'lineup'],
+      //     include: {
+      //       attributes: ['name'],
+      //     }
           
-        },
-      ],
+      //   },
+      // ],
     });
-    console.log(festivalData);
+    // console.log(festivalData);
     // Serialize data so the template can read it
     const festivals = festivalData.map((festivals) => festivals.get({ plain: true }));
 
@@ -29,6 +29,7 @@ router.get('/', async (req, res) => {
       user_name: req.session.username,
     });
   } catch (err) {
+    console.log(err);
     res.status(500).json(err);
   }
 });
