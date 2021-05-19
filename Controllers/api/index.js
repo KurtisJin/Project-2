68f6137f8aba491688e078e00f175cc0bc59e8b6
kurtisjin <cloudnine21@gmail.com>
2021-05-18 21:00:25 -0500

    fixed routes

--- Controllers/api/index.js
+++ Controllers/api/index.js
@@ -1,6 +1,7 @@
 const router = require('express').Router();
 const userRoutes = require('./userRoutes');
 const festivalRoutes = require('./festivalRoutes');
+// const logout = require('')
 
 router.use('/users', userRoutes);
 router.use('/festivals', festivalRoutes);
