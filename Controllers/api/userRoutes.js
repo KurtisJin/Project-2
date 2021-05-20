8c8b044f3d8d01538a3cdb11a922ec08ba88ca2d
Katherine Jackson <katherine.jackson206@gmail.com>
2021-05-19 21:06:47 -0500

    fixing build

--- Controllers/api/userRoutes.js
+++ Controllers/api/userRoutes.js
@@ -1,7 +1,5 @@
 const router = require('express').Router();
-const {
-  User
-} = require('../../models');
+const { User } = require('../../models');
 
 router.post('/', async (req, res) => {
   try {
@@ -20,18 +18,12 @@ router.post('/', async (req, res) => {
 
 router.post('/login', async (req, res) => {
   try {
-    const userData = await User.findOne({
-      where: {
-        email: req.body.email
-      }
-    });
+    const userData = await User.findOne({ where: { email: req.body.email } });
 
     if (!userData) {
       res
         .status(400)
-        .json({
-          message: 'Incorrect email or password, please try again'
-        });
+        .json({ message: 'Incorrect email or password, please try again' });
       return;
     }
 
@@ -40,20 +32,15 @@ router.post('/login', async (req, res) => {
     if (!validPassword) {
       res
         .status(400)
-        .json({
-          message: 'Incorrect email or password, please try again'
-        });
+        .json({ message: 'Incorrect email or password, please try again' });
       return;
     }
 
     req.session.save(() => {
       req.session.user_id = userData.id;
       req.session.logged_in = true;
-
-      res.json({
-        user: userData,
-        message: 'You are now logged in!'
-      });
+      
+      res.json({ user: userData, message: 'You are now logged in!' });
     });
 
   } catch (err) {
@@ -62,18 +49,14 @@ router.post('/login', async (req, res) => {
 });
 
 router.post('/logout', (req, res) => {
-  try {
-    if (req.session.logged_in) {
-      console.log(req.session.logged_in);
-      req.session.destroy(() => {
-        res.status(204).end();
-      });
-    } else {
-      res.status(404).end();
-    }
-  } catch (err) {
-    res.status(400).json(err);
-  }
+  if (req.session.logged_in) {
+    console.log(req.session.logged_in);
+    req.session.destroy(() => {
+      res.status(204).end();
+    });
+  } else {
+    res.status(404).end();
+  } 
 });
 
-    module.exports = router;
+module.exports = router;
