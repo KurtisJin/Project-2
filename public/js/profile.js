bfd4be4c479e2db297f3fdbda268fdc616b5b340
Jakers811 <akerstjustin@gmail.com>
2021-05-20 21:48:06 -0500

    Removed old code that was not being used

--- public/js/profile.js
+++ public/js/profile.js
@@ -1,47 +1,47 @@
-const newFormHandler = async (event) => {
-  event.preventDefault();
+// const newFormHandler = async (event) => {
+//   event.preventDefault();
 
-  const name = document.querySelector('#festival-name').value.trim();
-  const needed_funding = document.querySelector('#festival-funding').value.trim();
-  const description = document.querySelector('#festival-desc').value.trim();
+//   const name = document.querySelector('#festival-name').value.trim();
+//   const needed_funding = document.querySelector('#festival-funding').value.trim();
+//   const description = document.querySelector('#festival-desc').value.trim();
 
-  if (name && needed_funding && description) {
-    const response = await fetch(`/api/festivals`, {
-      method: 'POST',
-      body: JSON.stringify({ name, needed_funding, description }),
-      headers: {
-        'Content-Type': 'application/json',
-      },
-    });
+//   if (name && needed_funding && description) {
+//     const response = await fetch(`/api/festivals`, {
+//       method: 'POST',
+//       body: JSON.stringify({ name, needed_funding, description }),
+//       headers: {
+//         'Content-Type': 'application/json',
+//       },
+//     });
 
-    if (response.ok) {
-      document.location.replace('/profile');
-    } else {
-      alert('Failed to create festival');
-    }
-  }
-};
+//     if (response.ok) {
+//       document.location.replace('/profile');
+//     } else {
+//       alert('Failed to create festival');
+//     }
+//   }
+// };
 
-const delButtonHandler = async (event) => {
-  if (event.target.hasAttribute('data-id')) {
-    const id = event.target.getAttribute('data-id');
+// const delButtonHandler = async (event) => {
+//   if (event.target.hasAttribute('data-id')) {
+//     const id = event.target.getAttribute('data-id');
 
-    const response = await fetch(`/api/festivals/${id}`, {
-      method: 'DELETE',
-    });
+//     const response = await fetch(`/api/festivals/${id}`, {
+//       method: 'DELETE',
+//     });
 
-    if (response.ok) {
-      document.location.replace('/profile');
-    } else {
-      alert('Failed to delete festival');
-    }
-  }
-};
+//     if (response.ok) {
+//       document.location.replace('/profile');
+//     } else {
+//       alert('Failed to delete festival');
+//     }
+//   }
+// };
 
-document
-  .querySelector('.new-project-form')
-  .addEventListener('submit', newFormHandler);
+// document
+//   .querySelector('.new-project-form')
+//   .addEventListener('submit', newFormHandler);
 
-document
-  .querySelector('.project-list')
-  .addEventListener('click', delButtonHandler);
+// document
+//   .querySelector('.project-list')
+//   .addEventListener('click', delButtonHandler);
