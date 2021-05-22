b6ef0d4359233b6d83a0d329a25edfb9fcc8bca5
Katherine Jackson <katherine.jackson206@gmail.com>
2021-05-21 20:21:38 -0500

    fixed merge issue

--- Controllers/api/userRoutes.js
+++ Controllers/api/userRoutes.js
@@ -4,15 +4,9 @@ const { User } = require('../../models');
 router.post('/', async (req, res) => {
   try {
     const userData = await User.create(req.body);
-<<<<<<< HEAD
+
     console.log(userData.id);
-=======
-<<<<<<< HEAD
-    console.log(userData.id)
-=======
-    console.log(userData.id);
->>>>>>> 1a72117d92e1c960d500586fd127b1a195c9b832
->>>>>>> 54aca894495d09fc19c27923e7001f32dad69d32
+
     req.session.save(() => {
       req.session.user_id = userData.id;
       req.session.logged_in = true;
